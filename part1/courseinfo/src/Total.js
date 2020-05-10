a9d6f6f262647bb6c19dcb058d5ee8cc75075fb2
Alex Yu <alexjy@yahoo.com>
2020-05-10 13:44:10 -0400

    Part 1 first project

---
+++ part1/courseinfo/src/Total.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Total = (props) => {
+    return (
+        <p>Number of exercises {props.course.parts[0].exercises + props.course.parts[0].exercises + props.course.parts[0].exercises}</p>
+    )
+}
+
+export default Total
